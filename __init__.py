e5a70a70fd5c75fa4536f6248999cada27de1d08
Yousof-kayal <77901658+Yousof-kayal@users.noreply.github.com>
2021-04-26 20:43:16 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,19 +1,53 @@
 import os
-from flask import Flask, request, render_template, flash, url_for, send_from_directory, redirect, send_file
-from s3_function import list_files, download_file, upload_file
-import pyqrcode
 import boto3
+
+
+os.environ['AWS_PROFILE'] = "Profile1"
+os.environ['AWS_DEFAULT_REGION'] = "us-east-1"
+
+from flask import Flask, request, render_template, flash, url_for, send_from_directory, redirect, send_file
+import pyqrcode
 import io
 import base64
 import sqlite3
 import html
+import os
 
+global bucket
 app = Flask(__name__)
-app.secret_key = "key123"
+app.secret_key = "eih1zWk94BcZPJu6SMgZSVUydrersFR41bgWCItP"
 DOWNLOAD_CODE = "Codes"
 BUCKET = "qrgenerator-code"
 processed_text=''
 
+
+def upload_file(filename: object, bucket: object) -> object:
+    object_name = (f'{filename}')
+    s3_client = boto3.client('s3')
+    response = s3_client.upload_file(filename, bucket, object_name)
+
+    return response
+
+def download_file(filename, bucket):
+    """
+    Function to download a given file from an S3 bucket
+    """
+    s3 = boto3.resource('s3')
+    output = filename
+    s3.Bucket(bucket).download_file(filename, output)
+
+    return output
+
+def list_files(bucket):
+    """
+    Function to list files in a given S3 bucket
+    """
+    s3 = boto3.client('s3')
+    contents = []
+    for item in s3.list_objects(Bucket=bucket)['Contents']:
+        contents.append(item)
+
+    return contents
 
 @app.route('/')
 def index():
@@ -54,7 +88,7 @@ def download():
     if request.method == 'GET':
         output = download_file('www.google.com.png', BUCKET)
 
-        return send_file(output, as_attachment=True)
+        return send_file(output, as_attachment=False)
 
 
 # @app.route('/QR', methods= ['GET'])
